d9d46cf51d7fc6e93370d0395a480379c7361125
ElvisGraph <elvisnovaspaula@gmail.com>
2019-03-25 15:38:16 -0400

    morgan agregado

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 // Directorio Publico
 app.use(express.static('public'));
 
+app.get('/contact', (req, res) => {
+res.send('<h1>Contacto</h1>')
+});
+
+app.get('/test', (req, res) => {
+  res.send('<h1>Test</h1>')
+  });
+
 // Servidor
 app.listen(3000, () => {
   console.log('Servidor Iniciado!');
