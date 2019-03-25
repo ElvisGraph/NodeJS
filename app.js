bce3cf69355f180bef2d067a414c2605dc3cba76
ElvisGraph <elvisnovaspaula@gmail.com>
2019-03-25 16:45:21 -0400

    NodeJs V1

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 
-// Directorio Publico
-app.use(express.static('public'));
+// Ajuste
+app.set('AppName', 'NodeJS - Curso');
+app.set('Puesto', '4000');
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
 
-app.get('/contact', (req, res) => {
-res.send('<h1>Contacto</h1>')
+// Router
+app.get('/', (req, res) => {
+const data = [{name: 'Elvis'}, {name: 'Elba'}, {name: 'Jose'}, {name: 'Maria'}]
+res.render('index.ejs', {familia: data});
 });
 
-app.get('/test', (req, res) => {
-  res.send('<h1>Test</h1>')
-  });
+// app.use(express.static('public'));
+
 
 // Servidor
-app.listen(3000, () => {
-  console.log('Servidor Iniciado!');
+app.listen(app.get('Puesto'), () => {
+  console.log(app.get('AppName'));
+  console.log(app.get('Puesto'));
+
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+  host     : '198.20.126.130',
+  user     : 'mllhmbdf_web',
+  password : 'Winbinbo08'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 });
